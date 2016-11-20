81eec97138ac855d57095d871023f41714ddd321
Rin Davis <Rin@Rins-MacBook-Air.local>
2016-11-20 07:44:36 -0600

    first

---
+++ js/application.js
@@ -0,0 +1,24 @@
+// Wait till the browser is ready to render the game (avoids glitches)
+window.requestAnimationFrame(function () {
+  new GameManager(4, KeyboardInputManager, HTMLActuator, LocalScoreManager);
+});
+
+
+    var imageList = [
+        "Tiles/2.jpg",
+        "Tiles/4.jpg",
+        "Tiles/8.jpg",
+        "Tiles/16.jpg",
+        "Tiles/32.jpg",
+        "Tiles/64.jpg",
+        "Tiles/128.jpg",
+        "Tiles/256.jpg",
+        "Tiles/512.jpg",
+        "Tiles/1024.jpg",
+        "Tiles/2048.jpg"
+    ];
+    for(var i = 0; i < imageList.length; i++ ) 
+    {
+        var imageObject = new Image();
+        imageObject.src = imageList[i];
+    }
